9f2cc2087b0f31263b7b8f537101dfcbe820eb27
Martijn van Halderen <martijnmdesigns@hotmail.com>
2021-01-17 14:26:43 +0100

    frontend demo example components

---
+++ src/components/CardExampleBEM.vue
@@ -0,0 +1,60 @@
+<template>
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="userContainer">
+		<div class="userContainerWrapper">
+			<h3 class="userContainerWrapper__heading text-gray-900">
+				Example Tailwind.css using tailwind.classes
+			</h3>
+			<p class="userContainerWrapper__text userContainerWrapper__text--gray">
+				Personal details and application.
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+	.userContainer {
+		@apply shadow;
+		@apply overflow-hidden;
+
+		@screen sm {
+			@apply rounded-lg;
+		}
+
+		@screen md {
+			@apply bg-green-50;
+		}
+	}
+
+	.userContainerWrapper {
+		@apply px-4;
+		@apply py-5;
+
+		&__heading {
+			@apply text-lg;
+			@apply leading-6;
+			@apply font-medium;
+			@apply text-gray-900;
+		}
+
+		&__text {
+			@apply mt-1;
+			@apply max-w-2xl;
+			@apply text-sm;
+
+			&--gray {
+				@apply text-gray-500;
+			}
+		}
+
+		@screen sm {
+			@apply px-6;
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+		<!-- <img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+		<!-- <CardExample /> -->
+		<CardExampleBEM />
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import CardExample from '@/components/CardExample.vue';
+import CardExampleBEM from '@/components/CardExampleBEM.vue';
 
 export default defineComponent({
 	name: 'Home',
 	components: {
-		HelloWorld,
+		// HelloWorld,
+		// CardExample,
+		CardExampleBEM,
 	},
 });
 </script>
